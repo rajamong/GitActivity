bc4b880506915b5de9daefb33c1533df132945a3
rajamong <63121185+rajamong@users.noreply.github.com>
2021-04-15 17:02:35 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
+num1 = 4
+num2 = 2
+ 
+print(num1, "+", num2, "=", add(num1, num2))
+print(num1, "-", num2, "=", subtract(num1, num2))
+print(num1, "*", num2, "=", multiply(num1, num2))
+print(num1, "/", num2, "=", divide(num1, num2))
+       
