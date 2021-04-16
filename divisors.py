8606f674b8ff874bf46ba1d733d308dfc66f83d6
rajamong <63121185+rajamong@users.noreply.github.com>
2021-04-15 17:23:34 -0700

    Delete divisors.py

--- divisors.py
+++
@@ -1,8 +0,0 @@
-def findDivisors(num) :
-    divider = 1
-    while divider <= num :
-        if (num % divider==0) :
-            print(divider),
-        divider = divider + 1
-         
-findDivisors(24)
